c7c375dac7d1fdca9b6769261edfa1ebfbc47ef2
Anton Shvab <antonshwab@Antons-MacBook-Air.local>
2018-04-16 23:22:14 +0300

    init

---
+++ vue-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <SearchBar/>
+    <!-- <TableHeader/> -->
+    <!-- <Table/> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import SearchBar from './components/SearchBar';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    SearchBar,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-app/src/components/SearchBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <form
+      v-on:submit.prevent="getAccountTxs"
+      class="form-inline my-2 my-lg-0 col-sm-3">
+      <div class="input-group">
+        <input
+          type="text"
+          v-bind:placeholder='account' />
+        <div class="input-group-append">
+          <button
+            class="btn btn-dark btn-sm"
+            type="submit">
+            <span class="icon-user"></span> Get Account
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  data() {
+    return {
+      account: 'bittrex',
+    };
+  },
+  methods: {
+    // eslint-disable-next-line
+    getAccountTxs: event => console.log('from getAccountTxs:', event.target.value),
+  },
+};
+</script>
